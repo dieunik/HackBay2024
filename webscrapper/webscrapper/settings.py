5d8f9adf901e2e9f330a6b51ea0442e96ab96adb
Arthur Kölz <arthurkoelz@gmail.com>
2024-05-15 14:57:02 +0200

    feat: add the pipline in the settings.py so it gets executed on every newly scrapped offer. Also get rid of the .json previously used to store scraped offers

--- webscrapper/webscrapper/settings.py
+++ webscrapper/webscrapper/settings.py
@@ -12,7 +12,6 @@ BOT_NAME = "webscrapper"
 SPIDER_MODULES = ["webscrapper.spiders"]
 NEWSPIDER_MODULE = "webscrapper.spiders"
 
-
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = "webscrapper (+http://www.yourdomain.com)"
 
@@ -62,9 +61,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "webscrapper.pipelines.WebscrapperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+    "webscrapper.pipelines.MariaDBPipeline": 400,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
